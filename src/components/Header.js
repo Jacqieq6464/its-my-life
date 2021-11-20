1dccf81b8a841d2c27b6f88f5fc9770316cdf29c
jacqie williamson <jacqieq@me.com>
2021-11-20 21:39:22 +0000

    add title props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 //import React from 'react'
 
-const Header = () => {
+const Header = ({ title }) => {
   return (
     <header>
-      <h1>It's My Life Journal</h1>
+      <h1>{title}</h1>
     </header>
   );
 };
 
+Header.defaultProps = {
+  title: "It's My Life Journal",
+};
 export default Header;
