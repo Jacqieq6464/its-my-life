f7dcb68fcf5328583e0d3d61d06e2b495ed5d1ad
jacqie williamson <jacqieq@me.com>
2021-11-20 21:31:30 +0000

    import Header, cr Header tag, cr container class

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="App">
-      <h1>It's My Life Journal</h1>
+    <div className="container">
+      <Header />
     </div>
   );
 }
