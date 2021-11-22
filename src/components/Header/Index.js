42852f05d69d6ec6870a3965d24b6a6f895abac1
Cgordon-bee <81937022+Cgordon-bee@users.noreply.github.com>
2021-11-22 00:49:05 +0000

    added proptype to Button index

--- src/components/Header/Index.js
+++ src/components/Header/Index.js
@@ -1,16 +1,37 @@
 //import React from 'react'
-import Button from "../Buttons";
+import PropTypes from 'prop-types';
+import Button from "../Buttons"
 
 const Header = ({ title }) => {
+const onClick = () => {
+  console.log('click')
+}
+
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button />
+      {/* if we require more buttons, then add  after line 14*/}
+      <Button color='green' text= 'New Entry' onClick={onClick} />
     </header>
-  );
-};
+  )
+}
 
 Header.defaultProps = {
   title: "It's My Life Journal",
-};
+}
+
+{
+Header.protoType = {
+  title: PropTypes.string.isRequired,
+
+}
+
+
+}
+
+
+
+
+
+
 export default Header;
